93926d00d4b64fee603d4517a252befc1ae89da6
skyer9 <skyer9@gmail.com>
2021-07-24 12:41:06 +0900

    CORS 설정 변경

--- src/main/java/com/example/demo2/config/WebMvcConfig.java
+++ src/main/java/com/example/demo2/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
