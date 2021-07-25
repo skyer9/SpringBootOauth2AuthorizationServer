82148281bf78c5680d5a43e3ffc5f5b0fb46c340
skyer9 <skyer9@gmail.com>
2021-07-25 14:18:21 +0900

    비대칭 암호화를 이용한 토큰 암호화 /oauth/token_key 활성화

--- src/main/java/com/example/demo2/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/demo2/config/AuthorizationServerConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -12,8 +14,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 @Configuration
@@ -44,6 +49,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()") //allow check token
+                .tokenKeyAccess("isAnonymous()")
                 .allowFormAuthenticationForClients();
     }
 
@@ -54,10 +60,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
     }
 
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey(signKey);
+//        return converter;
+//    }
+
     @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+    public JwtAccessTokenConverter jwtAccessTokenConverter() throws Exception {
+
+        // 패스워드 저장 파일
+        String password = "";
+        ClassPathResource resource = new ClassPathResource("my-oauth-jwt.private");
+        InputStreamReader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.US_ASCII);
+        StringBuilder builder = new StringBuilder();
+        for(int c = reader.read(); c != -1; c = reader.read()){
+            builder.append((char) c);
+        }
+        password = builder.toString();
+
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new FileSystemResource("src/main/resources/my-oauth-jwt.jks"), password.toCharArray());
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("my-oauth-jwt"));
+
         return converter;
     }
 }
